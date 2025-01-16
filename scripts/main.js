1daeeef0fc6ecf3d107de1cddc5b5f17970508d9
gruguuoc <grugu@uoc.edu>
2025-01-16 21:42:12 +0100

    only one script is used as it is not necessary to have separated

--- scripts/main.js
+++ scripts/main.js
@@ -8,4 +8,19 @@ function setCurrentNavMenuElementBorder(){
     Array.from(firstChildren.children).filter(x => x.outerText === pageTitle)[0].children[0].classList.add('nav-menu-border');
 }
 
+function loadYTVideo(){
+    let htmlContent = `<iframe
+                        width="100%"
+                        height="100%"
+                        src="https://www.youtube.com/embed/TUd7JORZeWo?si=PAs6s9dZSZH7TlVW"
+                        title="YouTube video player"
+                        frameborder="0"
+                        allow="accelerometer;autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share"
+                        referrerpolicy="no-referrer"
+                        loading="lazy"
+                        allowfullscreen></iframe>`
+
+    document.getElementById('iframe-wrapper').innerHTML = htmlContent;
+}
+
 setCurrentNavMenuElementBorder();
