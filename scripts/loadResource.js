ab7cb110b18d098b72847b52fc4fa1d0a4ffc465
gruguuoc <grugu@uoc.edu>
2025-01-16 21:36:38 +0100

    re-working on img optimization

--- scripts/loadResource.js
+++ scripts/loadResource.js
@@ -1,5 +1,4 @@
 function loadYTVideo(){
-    console.log("hola");
     let htmlContent = `<iframe
                         width="100%"
                         height="100%"
@@ -7,7 +6,7 @@ function loadYTVideo(){
                         title="YouTube video player"
                         frameborder="0"
                         allow="accelerometer;autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share"
-                        referrerpolicy="strict-origin-when-cross-origin"
+                        referrerpolicy="no-referrer"
                         loading="lazy"
                         allowfullscreen></iframe>`
 
